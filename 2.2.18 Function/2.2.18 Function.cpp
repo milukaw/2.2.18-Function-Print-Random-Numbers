4230f630cecea39155953bf00926242e01b73bd0
SERWER <SERWER@DP-RT-SR-01>
2023-02-08 10:05:30 +0100

    Dodaj pliki projektów.

---
+++ 2.2.18 Function/2.2.18 Function.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void RandNumber(int num);
+
+int main()
+{
+    srand((unsigned) time(NULL));
+    RandNumber(10);
+}
+
+void RandNumber(int num)
+{
+    int i;
+    double random = rand() / (double)RAND_MAX;
+    std::cout << random << std::endl;
+    double prev_random = random;
+
+    for (i = 0; i < num; i++)
+    {
+        random = 1 - (prev_random * prev_random);
+        prev_random = random;
+        std::cout << random << std::endl;
+    }
+    
+}
